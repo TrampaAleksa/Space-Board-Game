ffe68e72e12b937c4707b3a0fb0da2627044a2df
Aleksa Trampa <alektramp@gmail.com>
2020-02-07 14:31:28 +0100

    Added healing and damaging fields

---
+++ Assets/Scripts/FieldEffects/EffectHealPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectHealPlayer : FieldEffect
+{
+    public float amountToHeal = 10;
+    public override void TriggerEffect()
+    {
+        InstanceManager.Instance.Get<HealthHandler>().HealPlayer(playersHandler.GetCurrentPlayer(), amountToHeal);
+        playersHandler.EndCurrentPlayersTurn();
+    }
+
+}
