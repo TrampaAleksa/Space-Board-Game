f371c1e1ea51f83164864e6cd05700aa5507a95e
Aleksa Trampa <alektramp@gmail.com>
2020-02-17 12:56:44 +0100

    Camera modes

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform target;
+
+    public float smoothSpeed = 0.305f;
+    public Vector3 offset;
+
+    public ICameraMode currentCameraMode;
+
+    private void Start()
+    {
+        currentCameraMode = new DefaultCameraMode();
+    }
+    private void LateUpdate()
+    {
+        currentCameraMode.UpdateCamera(offset, smoothSpeed);
+    }
+
+    public void SetCameraMode(ICameraMode mode)
+    {
+        currentCameraMode = mode;
+    }
+
+    public class DefaultCameraMode : ICameraMode
+    {
+        Transform target;
+        Transform camera;
+        public void UpdateCamera(Vector3 offset, float smoothSpeed)
+        {
+            target = InstanceManager.Instance.Get<PlayersHandler>().GetCurrentPlayer().transform;
+            camera = Camera.main.transform;
+            Vector3 desiredPosition = target.position + offset;
+            Vector3 smoothedPosition = Vector3.Lerp(camera.position, desiredPosition, smoothSpeed * Time.deltaTime);
+            camera.position = smoothedPosition;
+        }
+    }
+}
