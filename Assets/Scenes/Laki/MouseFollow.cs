cc199a3ee552a77263a4fe91885c28e115e8985d
unLa4ky <unLa4ky@users.noreply.github.com>
2020-02-19 01:51:20 +0100

    Added FreeLook

---
+++ Assets/Scenes/Laki/MouseFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseFollow : MonoBehaviour
+{
+    public float ScrollSpeed = 2;
+    public float speed=0.01f;
+    float Horizontal;
+    float Vertical;
+    Vector3 tmp;
+    void Update()
+    {
+        Horizontal = Input.GetAxis("Mouse X")*0.01f;
+        Vertical = Input.GetAxis("Mouse Y") * 0.01f;
+        tmp += new Vector3(Horizontal, 0, Vertical);
+
+        if (Input.mousePosition.y >= Screen.height * 0.99 || Input.mousePosition.y <= Screen.height * 0.01 || Input.mousePosition.x >= Screen.width * 0.99 || Input.mousePosition.x <= Screen.width * 0.01)
+        {
+            transform.position += tmp * Time.deltaTime * ScrollSpeed;
+        }
+    }
+}
