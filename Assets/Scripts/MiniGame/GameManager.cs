1c8b555d36c17b75fd0d5424198f848b08080e7f
unLa4ky <unLa4ky@users.noreply.github.com>
2020-02-24 21:55:24 +0100

    minor

--- Assets/Scripts/MiniGame/GameManager.cs
+++ Assets/Scripts/MiniGame/GameManager.cs
@@ -3,11 +3,7 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
-{
-    public GameObject pan; //panel
-    public Text winText;//na kraju levela tekst
-    public Button btn;//dugme na kraju levela
-
+{ 
     private float m_horizontalInput;
     private float m_verticalInput;
     private float m_steeringAngle;
@@ -18,73 +14,31 @@ public class GameManager : MonoBehaviour
     public float maxSteerAngle;
     public float motorForce;
 
-    int pom=4;
-    bool finishBtn;
-    bool fullFinish;
-
-    void Start()
-    {
-        pan.SetActive(false);
-        fullFinish = false;
-        Time.timeScale = 0;//Zamrzavamo vreme
-    }
-
-    void Update()
-    {
-        if (fullFinish == false)
-            if (Input.anyKeyDown)//pitam da li je dugme neko dole
-            {
-                Time.timeScale = 1;//Odmrzavam vreme
-            }
-    }
-
     private void FixedUpdate()
     {
-            GetInput();
+        GetInput();
+        Steer();
+        Accelerate();
+        UpdateWheelPoses();
     }
 
     public void GetInput()
     {
         m_horizontalInput = Input.GetAxis("Horizontal");
         m_verticalInput = Input.GetAxis("Vertical"); //Podesavanje osa za strelice
-        Steer();
     }
 
     private void Steer()
     {
-
         m_steeringAngle = maxSteerAngle * m_horizontalInput; //Ugao od pritiska strelice
+        Debug.Log(m_steeringAngle);
         frontPassengerW.steerAngle = frontDriverW.steerAngle = m_steeringAngle; //Prednji levi tocak 
-        if(pom==4) //sve je normalno
-            Accelerate(motorForce);
-        else if (pom == 3)// da bi postojao delay
-        {
-            if (frontDriverW.motorTorque > motorForce) 
-                Accelerate(frontDriverW.motorTorque - 1);//vrati ga na motorForce brzinu
-            else if (frontDriverW.motorTorque < motorForce)
-                Accelerate(frontDriverW.motorTorque + 1);//vrati ga na motorForce brzinu
-            else if (frontDriverW.motorTorque == motorForce)
-            {
-                pom = 4;
-                Accelerate(motorForce);
-            }
-        }
-        if (pom == 1) //pokupili smo plavu
-        {
-            pom = 3;
-            Accelerate(10);
-        }
-        else if (pom == 2) //pokupili smo zelenu
-        {
-            pom = 3;
-            Accelerate(frontDriverW.motorTorque * 2);
-        }
+        
     }
 
-    public void Accelerate(float ubrzaj)
+    public void Accelerate()
     {
-        frontPassengerW.motorTorque = frontDriverW.motorTorque = m_verticalInput *ubrzaj;
-        UpdateWheelPoses();
+        frontPassengerW.motorTorque = frontDriverW.motorTorque = m_verticalInput *motorForce;
     }
 
     private void UpdateWheelPoses()
@@ -107,60 +61,31 @@ public class GameManager : MonoBehaviour
 
     public void PickUp(Color colorPickUp) // desavanja pri kupljenju poena sa prom
     {
-        if (colorPickUp == Color.blue)
-        {
-            pom = 1;
-        }
-        else if (colorPickUp == Color.black)
-            Die();
-        else { pom = 2;}
     }
     public void CompleteLevel()
     {
-        finishBtn = true;
-        winText.text = "Finish!" + "\n";
-        btn.transform.GetChild(0).GetComponent<Text>().text = "Next level";//Promena teksta dugmeta
         Stop();
     }
 
      public void completeAll()
      {
-         finishBtn = true;
-         btn.transform.GetChild(0).GetComponent<Text>().text = "Finish";
-         winText.text = "You have finished all levels!";
          Stop();
      }
 
     public void CheckPoint()
     {
-        pom = 1;
     }
     public void Die()
     {
-        finishBtn = false;
-        btn.transform.GetChild(0).GetComponent<Text>().text = "Main menu";
-        winText.text = "Better luck next time";
         Stop();
     }
 
     public void Stop()
     {
-        pan.SetActive(true);//Aktiviran panel
-        fullFinish = true;//Boolean postavljam da je gotov level
-        btn.onClick.AddListener(() => ButtonClicked()); //Slusa da li je kliknuto na dugme
-        Time.timeScale = 0; //Gasi vreme
     }
 
     public void ButtonClicked()
     {
-        if (finishBtn == false)
-            SceneManager.LoadScene("Mainmenu");
-        if (finishBtn == true) {
-            if (SceneManager.GetActiveScene().buildIndex == 3)//Zavrsili smo poslednji nivo(index treceg)
-                SceneManager.LoadScene("Mainmenu");
-            else
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);//naredni level
-            }
     }
     
 }
