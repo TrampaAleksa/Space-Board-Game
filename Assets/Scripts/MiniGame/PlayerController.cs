2a994ec23063f8f98d597d6166b6f37e02ef8a5c
Aleksa Trampa <alektramp@gmail.com>
2020-03-01 21:13:45 +0100

    Added death line, removed terrain, skybox and all materials for that asset.

--- Assets/Scripts/MiniGame/PlayerController.cs
+++ Assets/Scripts/MiniGame/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+
 public partial class PlayerController : MonoBehaviour
 {
     private int lap;
@@ -14,10 +15,10 @@ public partial class PlayerController : MonoBehaviour
     private Text[] pText;
     public string nameOfInputHorizontal;
     public string nameOfInputVertical;
+
     public WheelCollider frontLeftW, frontRightW,
                          rearLeftW, rearRightW;
-    public Transform frontLeftT, frontRightT,
-                     rearLeftT, rearRightT;
+
     public float maxSteerAngle;
     public float motorForce;
     public float numberOfLaps;
@@ -28,10 +29,10 @@ public partial class PlayerController : MonoBehaviour
     {
         Instance = this;
     }
+
     private void Start()
     {
         WheelCollider[] wheelColliders = gameObject.GetComponentsInChildren<WheelCollider>();
-        Transform[] transforms = gameObject.GetComponentsInChildren<Transform>();
         for (int i = 0; i < 4; i++)
         {
             if (wheelColliders[i].name == "WC_FRONTLEFT")
@@ -42,20 +43,12 @@ public partial class PlayerController : MonoBehaviour
                 rearLeftW = wheelColliders[i];
             else if (wheelColliders[i].name == "WC_REARRIGHT")
                 rearRightW = wheelColliders[i];
-
-            if (transforms[i + 1].name == "WESD_FRONTLEFT")
-                frontLeftT = transforms[i + 1];
-            if (transforms[i + 1].name == "WESD_FRONTRIGHT")
-                frontRightT = transforms[i + 1];
-            if (transforms[i + 1].name == "WESD_REARLEFT")
-                rearLeftT = transforms[i + 1];
-            if (transforms[i + 1].name == "WESD_REARRIGHT")
-                rearRightT = transforms[i + 1];
         }
         pText = GameObject.Find("Canvas").GetComponentsInChildren<Text>();
         num = int.Parse(gameObject.name);
         pText[num - 1].text = "Lap: " + lap.ToString() + "/" + numberOfLaps;
     }
+
     private void FixedUpdate()
     {
         if (!playerFinishRace)
@@ -64,9 +57,8 @@ public partial class PlayerController : MonoBehaviour
             Steer();
             Accelerate();
         }
-        UpdateWheelPoses();
-        
     }
+
     public void GetInput()
     {
         m_horizontalInput = Input.GetAxis(nameOfInputHorizontal);
@@ -76,7 +68,7 @@ public partial class PlayerController : MonoBehaviour
     private void Steer()
     {
         m_steeringAngle = maxSteerAngle * m_horizontalInput; //Ugao od pritiska strelice
-        frontRightW.steerAngle = frontLeftW.steerAngle = m_steeringAngle; //Prednji levi tocak 
+        frontRightW.steerAngle = frontLeftW.steerAngle = m_steeringAngle; //Prednji levi tocak
     }
 
     public void Accelerate()
@@ -84,33 +76,16 @@ public partial class PlayerController : MonoBehaviour
         frontRightW.motorTorque = frontLeftW.motorTorque = m_verticalInput * motorForce;
     }
 
-    private void UpdateWheelPoses()
-    {
-        UpdateWheelPose(frontLeftW, frontLeftT);
-        UpdateWheelPose(frontRightW, frontRightT);
-        UpdateWheelPose(rearLeftW, rearLeftT);
-        UpdateWheelPose(rearRightW, rearRightT);
-    }
-    private void UpdateWheelPose(WheelCollider _collider, Transform _transform)
-    {
-        Vector3 _pos = _transform.position; // Trenutna pozicija
-        Quaternion _quat = _transform.rotation; // Tr rot
-
-        _collider.GetWorldPose(out _pos, out _quat); //https://docs.unity3d.com/ScriptReference/WheelCollider.GetWorldPose.html
-        _transform.position = _pos; //Dodela
-        _transform.rotation = _quat; //Dodela
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Finish")
         {
-            if (numberOfLaps!=lap) 
+            if (numberOfLaps != lap)
             {
                 if (!beforeFinishPass)
                 {
                     FinishedLap();
-                } 
+                }
             }
             else
             {
@@ -119,13 +94,18 @@ public partial class PlayerController : MonoBehaviour
                 FinishedLap();
             }
         }
+        if (other.tag == "Death")
+        {
+            Debug.Log("Died");
+        }
 
         if (other.tag == "NextField")
         {
             beforeFinishPass = false;
         }
     }
-    private void FinishedLap() 
+
+    private void FinishedLap()
     {
         lap++;
         pText[num - 1].text = "Lap: " + (lap - 1).ToString() + "/" + numberOfLaps;
