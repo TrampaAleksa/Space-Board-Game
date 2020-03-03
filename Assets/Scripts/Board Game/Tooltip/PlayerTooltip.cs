4a7ec384a910adddd2047368fe82c5002a4642fe
Aleksa Trampa <alektramp@gmail.com>
2020-03-03 10:04:05 +0100

    tooltip that follows a player added

---
+++ Assets/Scripts/Board Game/Tooltip/PlayerTooltip.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTooltip : MonoBehaviour
+{
+    private Tooltip tooltip;
+    public GameObject tooltipText;
+    public Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tooltip = tooltipText.GetComponent<Tooltip>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tooltipText.transform.position = transform.position + offset;
+    }
+}
