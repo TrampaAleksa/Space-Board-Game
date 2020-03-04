fe4924d9bf5843350dd5c76b123907c04bbc4506
unLa4ky <unLa4ky@users.noreply.github.com>
2020-03-04 03:03:04 +0100

    Refactoring code and added Scene Manager for load board game scene. Added class for player and added different solution for id.

--- Assets/Mini Games/car race minigame/Scripts/GameManager.cs
+++ Assets/Mini Games/car race minigame/Scripts/GameManager.cs
@@ -1,43 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public PlayerBoardState[] playerBoardStates;
 
     private int counterRankInc = 1;
-    public int counterRankDec = 4;
+    private int counterRankDec = 4;
     private int numberOfPlayersFinished=0;
 
     public static GameManager Instance;
+
     private void Awake()
     {
         Instance = this;
     }
+    public string ReturnName(int number)
+    {
+        return playerBoardStates[number].name;
+    }
 
     public void Win(int number) 
     {
-            StateOfFinishGame(counterRankInc, number);
-            counterRankInc++;
+        StateOfFinishGame(counterRankInc, number);
+        counterRankInc++;
         
     }
     public void Lose(int number)
     {
-            StateOfFinishGame(counterRankDec, number);
-            counterRankDec--;
+        StateOfFinishGame(counterRankDec, number);
+        counterRankDec--;
     }
     private void StateOfFinishGame(int count, int number) 
     {
-        playerBoardStates[number - 1].rank = count;
+        playerBoardStates[number].rank = count;
         numberOfPlayersFinished++;
-        if (numberOfPlayersFinished == 4)
+        if (numberOfPlayersFinished == 3)
             GameFinished();
-        Debug.Log("player rank: " + playerBoardStates[number-1].rank);
-        Debug.Log("pleyer" + number);
     }
     private void GameFinished() 
     {
-        Time.timeScale = 0;
+        SceneManager.LoadScene(1);
     }
 }
