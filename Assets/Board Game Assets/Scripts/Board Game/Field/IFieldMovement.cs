facd4f7e590fbda046913c8b82e92888f69dc70f
Aleksa Trampa <alektramp@gmail.com>
2020-03-06 10:41:42 +0100

    Created interface for field movement methods

---
+++ Assets/Board Game Assets/Scripts/Board Game/Field/IFieldMovement.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IFieldMovement
+{
+    void SetCurrentField(Field fieldToSetTo, GameObject player);
+
+    void TeleportPlayerToField(GameObject player, Field field);
+
+    void SwapTwoPlayers(PlayerMovement playerMovement1, PlayerMovement playerMovement2);
+}
