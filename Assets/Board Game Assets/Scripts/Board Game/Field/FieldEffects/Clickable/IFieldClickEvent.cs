91f6aa719f82ab91efbd55f785c9f0480f72aa51
Aleksa Trampa <alektramp@gmail.com>
2020-03-11 12:26:48 +0100

    folder restructure in field effects

--- Assets/Board Game Assets/Scripts/Board Game/Field/FieldEffects/Clickable/IFieldClickEvent.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IFieldClickEvent
-{
-    void FieldClickAction(IClickEvent click, GameObject hit);
-}
