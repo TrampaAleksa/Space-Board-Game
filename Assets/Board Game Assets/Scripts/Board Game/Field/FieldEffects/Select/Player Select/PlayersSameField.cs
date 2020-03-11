c4ae01fecd6b611bb2edceddba4a349982b34f49
Aleksa Trampa <alektramp@gmail.com>
2020-03-11 18:38:38 +0100

    fixed bug taht locks the game in place if all 4 players are on the same field when trying to swap

---
+++ Assets/Board Game Assets/Scripts/Board Game/Field/FieldEffects/Select/Player Select/PlayersSameField.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayersSameField
+{
+    public static bool AllPlayersOnSameField()
+    {
+        GameObject[] players = InstanceManager.Instance.Get<PlayersHandler>().gameObjects;
+        bool sameField = false;
+        if (players[0].GetComponent<PlayerMovement>().currentPlayerField.IndexInPath ==
+            players[1].GetComponent<PlayerMovement>().currentPlayerField.IndexInPath)
+        {
+            if (players[0].GetComponent<PlayerMovement>().currentPlayerField.IndexInPath ==
+            players[2].GetComponent<PlayerMovement>().currentPlayerField.IndexInPath)
+            {
+                if (players[0].GetComponent<PlayerMovement>().currentPlayerField.IndexInPath ==
+                players[3].GetComponent<PlayerMovement>().currentPlayerField.IndexInPath)
+                {
+                    sameField = true;
+                }
+            }
+        }
+
+        return sameField;
+    }
+}
--- Assets/Board Game Assets/Scripts/Board Game/Field/FieldEffects/Select/Player Select/Swap Places/EffectSwapPlaces.cs
+++ Assets/Board Game Assets/Scripts/Board Game/Field/FieldEffects/Select/Player Select/Swap Places/EffectSwapPlaces.cs
@@ -7,6 +7,12 @@ public class EffectSwapPlaces : SelectPlayerEffect
 {
     public override void TriggerEffect()
     {
+        if (PlayersSameField.AllPlayersOnSameField())
+        {
+            InstanceManager.Instance.Get<TooltipHandler>().ShowInfoTooltip("All players are on the same field!");
+            InstanceManager.Instance.Get<TurnHandler>().EndCurrentPlayersTurn();
+            return;
+        }
         GenericSelectTrigger();
         GenericTriggerEffect();
         print("Swap places with someone!");
