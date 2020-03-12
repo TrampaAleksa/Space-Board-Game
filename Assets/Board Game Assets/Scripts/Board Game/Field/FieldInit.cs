7cdaf61fbee9d38390bc51031522009078e4d3ab
unLa4ky <unLa4ky@users.noreply.github.com>
2020-03-12 15:00:30 +0100

    Added path for Board game pt2. and little change canvas UI.

--- Assets/Board Game Assets/Scripts/Board Game/Field/FieldInit.cs
+++ Assets/Board Game Assets/Scripts/Board Game/Field/FieldInit.cs
@@ -5,32 +5,35 @@ using UnityEngine;
 
 public class FieldInit : IInitialize
 {
-    public PathCreator pathCreator;
+    private PathCreator pathCreator;
 
     private GenericObjectArray fields;
-    private int interval;
+    private float interval;
 
     public FieldInit(GenericObjectArray fields)
     {
+        this.pathCreator = GameObject.Find("path").GetComponent<PathCreator>();
         this.fields = fields;
         Debug.Log(pathCreator.path.length);
-        this.interval = (int)pathCreator.path.length/50;
+        this.interval = pathCreator.path.length / 54;
+        Debug.Log(this.interval);
 
     }
 
     public IInitialize Initialize()
     {
         fields.gameObjects = GameObject.FindGameObjectsWithTag("Field");
+        float tmp = interval;
         int i = 0;
         foreach (var field in fields.gameObjects)
         {
             field.tag = "Untagged";
             field.AddComponent<Field>();
             field.GetComponent<Field>().InitialSetUpField(i);
+            field.transform.position = pathCreator.path.GetPointAtDistance(interval += tmp);
             Vector3 _lookDirection = fields.MemberWithIndex(i + 1).transform.position - field.transform.position;
             Quaternion _rot = Quaternion.LookRotation(_lookDirection, Vector3.up);
             field.transform.rotation = Quaternion.Lerp(field.transform.rotation, _rot, 1);
-            field.transform.position = pathCreator.path.GetPoint(interval*i);
             Debug.Log(field.transform.position);
             i++;
         }
