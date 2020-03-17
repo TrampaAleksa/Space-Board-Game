8e52ced162bee30329aacc0b896b40cc6af6d088
Aleksa Trampa <alektramp@gmail.com>
2020-03-17 16:42:18 +0100

    Added mine triggered activity tooltip

--- Assets/Board Game Assets/Scripts/Board Game/Field/FieldEffects/Select/Field Select/Mine/Mine.cs
+++ Assets/Board Game Assets/Scripts/Board Game/Field/FieldEffects/Select/Field Select/Mine/Mine.cs
@@ -8,9 +8,9 @@ public class Mine : FieldEffect
 
     public override void TriggerEffect()
     {
-        print("YOU STEPPED ON A MINE");
         GameObject player = InstanceManager.Instance.Get<PlayersHandler>().GetCurrentPlayer();
         InstanceManager.Instance.Get<HullHandler>().DamagePlayer(player, mineDamage);
+        DisplayInActivityHistory();
         InstanceManager.Instance.Get<FieldEffectHandler>().RemoveEffectFromField(gameObject, this);
         Destroy(this);
     }
@@ -18,5 +18,14 @@ public class Mine : FieldEffect
     public override void FinishedEffect()
     {
         throw new System.NotImplementedException();
+    }
+
+     private void DisplayInActivityHistory()
+    {
+        string message = new ATMineTriggered(
+           InstanceManager.Instance.Get<PlayersHandler>().GetCurrentPlayer(),
+           (int)mineDamage
+           ).BuildActivityTooltip();
+        InstanceManager.Instance.Get<ActivityHistoryHandler>().ShowActivityTooltipMessage(message);
     }
 }
---
+++ Assets/Board Game Assets/Scripts/Board Game/Tooltip/Activity History/Activities/ATMineTriggered.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ATMineTriggered :  IBuildActivityTooltip
+{
+    private int value;
+    private GameObject player;
+
+    public ATMineTriggered(GameObject player, int value)
+    {
+        this.player = player;
+        this.value = value;
+    }
+
+    public string BuildActivityTooltip()
+    {
+        string playerName = player.GetComponent<PlayerName>().playerName.text;
+        string color = player.GetComponent<Player>().color;
+        string tooltipMessage = "";
+
+        tooltipMessage += RichTextBuilder.AddTagToString(playerName, "color", color);
+        tooltipMessage += " Stepped on a mine.  ";
+        tooltipMessage += RichTextBuilder.AddTagToString(value.ToString(), "color", color);
+        tooltipMessage += " damage";
+        return tooltipMessage;
+    }
+}
