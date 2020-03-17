c3359de191596ee06efeff634e8f1c30bedab97d
Aleksa Trampa <alektramp@gmail.com>
2020-03-17 17:13:46 +0100

    Move in direction activity tooltip

--- Assets/Board Game Assets/Scripts/Board Game/Field/FieldEffects/Movement/EffectMoveBackwards.cs
+++ Assets/Board Game Assets/Scripts/Board Game/Field/FieldEffects/Movement/EffectMoveBackwards.cs
@@ -12,8 +12,19 @@ public class EffectMoveBackwards : FieldEffect, IGenericFieldEffect
     {
         GameObject currentPlayer = InstanceManager.Instance.Get<PlayersHandler>().GetCurrentPlayer();
         int fieldsToMove = Random.Range(1, 7);
+        DisplayInActivityHistory(fieldsToMove, "backwards");
         InstanceManager.Instance.Get<TooltipHandler>().ShowPlayersTooltip(currentPlayer, "-" + fieldsToMove + " fields");
         InstanceManager.Instance.Get<MovementHandler>().MoveNFields(-fieldsToMove, currentPlayer);
         InstanceManager.Instance.Get<FieldEffectHandler>().TriggerEffectFinishedEvents(gameObject);
+    }
+
+     private void DisplayInActivityHistory(int fieldsToMove, string direction)
+    {
+        string message = new ATMoveInDirection(
+           InstanceManager.Instance.Get<PlayersHandler>().GetCurrentPlayer(),
+           fieldsToMove,
+           direction
+           ).BuildActivityTooltip();
+        InstanceManager.Instance.Get<ActivityHistoryHandler>().ShowActivityTooltipMessage(message);
     }
 }
---
+++ Assets/Board Game Assets/Scripts/Board Game/Tooltip/Activity History/Activities/AtMoveInDirection.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ATMoveInDirection : IBuildActivityTooltip
+{
+    private int value;
+    private GameObject player;
+    private string direction;
+
+    public ATMoveInDirection(GameObject player, int value, string direction)
+    {
+        this.player = player;
+        this.value = value;
+        this.direction = direction;
+    }
+
+    public string BuildActivityTooltip()
+    {
+        string playerName = player.GetComponent<PlayerName>().playerName.text;
+        string color = player.GetComponent<Player>().color;
+        string tooltipMessage = "";
+
+        tooltipMessage += RichTextBuilder.AddTagToString(playerName, "color", color);
+        tooltipMessage += " Moved  ";
+        tooltipMessage += RichTextBuilder.AddTagToString(value.ToString(), "color", color);
+        tooltipMessage += " fields ";
+        tooltipMessage += direction;
+
+        return tooltipMessage;
+    }
+}
