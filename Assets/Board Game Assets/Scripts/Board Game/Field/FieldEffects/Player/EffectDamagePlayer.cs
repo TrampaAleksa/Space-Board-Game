c1ab8a860b2a002013ceec893e62fa87a54d7761
Aleksa Trampa <alektramp@gmail.com>
2020-03-17 12:24:52 +0100

    Added prototype activity history for player damage effect

--- Assets/Board Game Assets/Scripts/Board Game/Field/FieldEffects/Player/EffectDamagePlayer.cs
+++ Assets/Board Game Assets/Scripts/Board Game/Field/FieldEffects/Player/EffectDamagePlayer.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EffectDamagePlayer : FieldEffect , IGenericFieldEffect
+public class EffectDamagePlayer : FieldEffect, IGenericFieldEffect
 {
     public float damageAmount = 20;
 
     public override void FinishedEffect()
     {
+        string message = new PlayerStatehangedActivityTooltip(
+            InstanceManager.Instance.Get<PlayersHandler>().GetCurrentPlayer().GetComponent<PlayerName>().playerName.text,
+            (int)damageAmount,
+            "damage",
+            "took"
+            ).BuildActivityTooltip();
+        InstanceManager.Instance.Get<ActivityHistoryHandler>().ShowActivityTooltipMessage(message);
         InstanceManager.Instance.Get<TurnHandler>().EndCurrentPlayersTurn();
     }
 
---
+++ Assets/Board Game Assets/Scripts/Board Game/Field/FieldEffects/Player/PlayerStatehangedActivityTooltip.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatehangedActivityTooltip : IBuildActivityTooltip
+{
+    private string playerName;
+    private int value;
+    private string valueName;
+    private string message;
+
+    public PlayerStatehangedActivityTooltip(string playerName, int value, string valueName, string message)
+    {
+        this.playerName = playerName;
+        this.value = value;
+        this.valueName = valueName;
+        this.message = message;
+    }
+
+    public string BuildActivityTooltip()
+    {
+        string tooltipMessage = "";
+
+        tooltipMessage += RichTextBuilder.AddTagToString(playerName, "color", "red");
+        tooltipMessage += " " + message;
+        tooltipMessage += " " + RichTextBuilder.AddTagToString(value.ToString(), "color", "red");
+        tooltipMessage += " " + valueName;
+        return tooltipMessage;
+    }
+}
