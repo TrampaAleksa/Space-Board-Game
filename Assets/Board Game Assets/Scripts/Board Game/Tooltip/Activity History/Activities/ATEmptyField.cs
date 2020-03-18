91674363488f8266e6d3078ab9b551be90da5ccc
Aleksa Trampa <alektramp@gmail.com>
2020-03-18 13:37:18 +0100

    Empty field activity tooltip

---
+++ Assets/Board Game Assets/Scripts/Board Game/Tooltip/Activity History/Activities/ATEmptyField.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ATEmptyField : IBuildActivityTooltip
+{
+    private GameObject _player;
+
+    public ATEmptyField(GameObject player)
+    {
+        this._player = player;
+    }
+
+    public string BuildActivityTooltip()
+    {
+        string playerName = _player.GetComponent<PlayerName>().playerName.text;
+        string color = _player.GetComponent<Player>().color;
+
+        string tooltipMessage = "";
+
+        tooltipMessage += RichTextBuilder.AddTagToString(playerName, "color", color);
+        tooltipMessage += " landed on an empty field ";
+        return tooltipMessage;
+    }
+}
+
