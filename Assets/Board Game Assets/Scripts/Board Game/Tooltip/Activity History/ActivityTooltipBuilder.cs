5b281b6483ab1676c66cc08752291e692bf81e40
Aleksa Trampa <alektramp@gmail.com>
2020-03-19 14:43:45 +0100

    adding activity tooltip builder for easily building tooltips instead of a lot of repeated code

---
+++ Assets/Board Game Assets/Scripts/Board Game/Tooltip/Activity History/ActivityTooltipBuilder.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivityHistoryTooltipHandler : MonoBehaviour
+{
+    public ActivityHistoryTooltipHandler DisplayAT(GameObject player)
+    {
+
+        
+        return this;
+    }
+
+    public ActivityHistoryTooltipHandler DisplayAT(GameObject player1, GameObject player2)
+    {
+
+
+        return this;
+    }
+    
+    public ActivityHistoryTooltipHandler DisplayAT(GameObject player1, float value)
+    {
+
+
+        return this;
+    }
+    
+    public ActivityHistoryTooltipHandler DisplayAT(GameObject player1, GameObject player2, float value)
+    {
+
+
+        return this;
+    }
+}
