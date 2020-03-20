4351aa715f4d2136988f2997b2fd9c37d7cd4aa3
Aleksa Trampa <alektramp@gmail.com>
2020-03-20 17:57:12 +0100

    Added working place mine selection effect without dependency on seelct effect parent class

--- Assets/Board Game Assets/Scripts/Board Game/Field/FieldEffects/Select/Field Select/Mine/EffectPlaceMine.cs
+++ Assets/Board Game Assets/Scripts/Board Game/Field/FieldEffects/Select/Field Select/Mine/EffectPlaceMine.cs
@@ -1,19 +1,25 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EffectPlaceMine : SelectFieldEffect , IGenericFieldEffect
+public class EffectPlaceMine : FieldEffect , IGenericFieldEffect
 {
+    private void Awake()
+    {
+        gameObject.AddComponent<GenericFieldSelectEffect>();
+        gameObject.AddComponent<MineFieldSelectionEvent>();
+    }
+
     public override void TriggerEffect()
     {
-        selectionEffect = new MineFieldSelectionEvent(gameObject);
-        print("Place a mine!");
-        GenericSelectTrigger();
     }
 
     public override void FinishedEffect()
     {
-        gameObject.GetComponent<SelectEffect>().FinishedSelecting();
+       // gameObject.GetComponent<SelectEffect>().FinishedSelecting();
         InstanceManager.Instance.Get<TurnHandler>().EndCurrentPlayersTurn();
     }
+
+  
 }
