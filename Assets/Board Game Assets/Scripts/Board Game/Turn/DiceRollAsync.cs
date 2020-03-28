ac6467ca5779a0646381571914751846eb799af1
Aleksa Trampa <alektramp@gmail.com>
2020-03-28 12:59:23 +0100

    Added new sound files, could be implemented somewhere. Changed Dice roll to work with async tasks instead of coroutines

---
+++ Assets/Board Game Assets/Scripts/Board Game/Turn/DiceRollAsync.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class DiceRollAsync 
+{
+    public Dice dice;
+    private int minimumDiceNumber = 1;
+    private int maximumDiceNumber = 6;
+
+    private float diceAnimationSpeed = 100f;
+    private DiceRollHandler diceRollHandler { get; set; }
+
+    public DiceRollAsync()
+    {
+        dice = new Dice(
+            minimumDiceNumber,
+            maximumDiceNumber,
+            diceAnimationSpeed,
+            Resources.LoadAll<Sprite>("DiceSides/"),
+            GameObject.Find("Dice").GetComponent<Image>()
+        );
+        diceRollHandler = InstanceManager.Instance.Get<DiceRollHandler>();
+    }
+
+    public async Task<int> RollDiceTheDiceAnim()
+    {
+        try
+        {
+            int randomDiceSide = 0;
+            AudioManager.Instance.PlaySound("diceRoll");
+            for (int i = 0; i <= 20; i++)
+            {
+                randomDiceSide = Random.Range(dice.MinimumDiceNumber-1, dice.MaximumDiceNumber);
+
+                dice.DiceImage.sprite = dice.DiceSides[randomDiceSide];
+
+                await Task.Delay((int)dice.DiceAnimationSpeed);
+            }
+            int numberRolled = randomDiceSide + 1;
+            diceRollHandler.numberRolled = numberRolled;
+            return numberRolled;
+        }
+        catch
+        {
+            Debug.Log("error");
+        }
+
+        return 0;
+    }
+
+}
