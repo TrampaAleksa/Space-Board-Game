ecdc5b6ce394ea39167aba3a0dc483c0f73e0c17
Aleksa Trampa <alektramp@gmail.com>
2020-04-01 12:11:09 +0200

    properties into player board state Scriptable object

--- Assets/Board Game Assets/Scripts/Board Game/Board State/PlayerBoardState.cs
+++ Assets/Board Game Assets/Scripts/Board Game/Board State/PlayerBoardState.cs
@@ -5,13 +5,12 @@ using UnityEngine;
 [CreateAssetMenu(fileName ="PlayerBoardState", menuName ="BoardStateSO")]
 public class PlayerBoardState : ScriptableObject
 {
-    public float fuel;
-    public float hull;
-    public int pathIndex;
-    public int checkpointIndex;
-    public int rank;
-    public int turnsToSkip;
+    public float fuel { get; set; }
+    public float hull { get; set; }
+    public int pathIndex { get; set; }
+    public int checkpointIndex { get; set; }
+    public int rank { get; set; }
+    public int turnsToSkip { get; set; }
 
-    public string playerName;
-    
+    public string playerName { get; set; }
 }
