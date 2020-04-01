278486c2d9e7c2055199b7328956512126e39132
Aleksa Trampa <alektramp@gmail.com>
2020-04-01 17:41:14 +0200

    Added mini game handler class for switching to random mini games after given number of turns

---
+++ Assets/Board Game Assets/Scripts/Board Game/Mini Game Trigger/MiniGameHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MiniGameHandler : MonoBehaviour
+{
+    public int turnsBeforeTriggering = 8;
+    public string[] miniGameSceneNames;
+    public int turnsElapsed = 0;
+    
+    public void TryTriggerMiniGame()
+    {
+        if (turnsElapsed < turnsBeforeTriggering)
+        {
+            turnsElapsed++;
+        }
+        else
+        {
+            SwitchToRandomMiniGame(miniGameSceneNames);
+            turnsElapsed = 0;
+        }
+    }
+    
+    public void SwitchToRandomMiniGame(string[] sceneNamesArray)
+    {
+        if (sceneNamesArray.Length > 0)
+        {
+            var randomIndex = Random.Range(0, sceneNamesArray.Length);
+            BoardStateHandler.Instance.SaveBoardState();
+            SceneManager.LoadScene(sceneNamesArray[randomIndex]);
+        }
+    }
+}
