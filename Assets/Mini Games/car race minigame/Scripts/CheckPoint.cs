aa5278f5a5390b82162432a13c15ac956a5940a0
unLa4ky <unLa4ky@users.noreply.github.com>
2020-04-01 19:12:35 +0200

    Add checkpoint without count player. in minigame rank

---
+++ Assets/Mini Games/car race minigame/Scripts/CheckPoint.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+public class CheckPoint : MonoBehaviour
+{
+    public PathCreator pathCreator;
+    public int numberOfCheckpoint;
+    public float distance;
+    public int numberOfPlayerCross=0;
+    public int checkpointIndex=0;
+
+    void Start()
+    {
+        pathCreator=GameObject.FindWithTag("Field").GetComponent<PathCreator>();
+        distance=pathCreator.path.length/numberOfCheckpoint;
+        UpdatePosition();
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        numberOfPlayerCross++;
+        if(numberOfPlayerCross==4)
+            UpdatePosition();
+    }
+    private void UpdatePosition()
+    {
+        checkpointIndex++;
+        gameObject.transform.position=pathCreator.path.GetPointAtDistance(distance*checkpointIndex);
+        gameObject.transform.localRotation=pathCreator.path.GetRotationAtDistance(distance*checkpointIndex);
+        gameObject.transform.localEulerAngles= new Vector3 (transform.localEulerAngles.x,transform.localEulerAngles.y, 0);
+    }
+    
+}
