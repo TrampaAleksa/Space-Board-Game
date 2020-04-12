83e3bb9db66aade8ddf1d308006ea096d02f18ae
unLa4ky <unLa4ky@users.noreply.github.com>
2020-04-13 01:15:01 +0200

    Add Mini map controller

---
+++ Assets/Mini Games/car race minigame/Scripts/MiniMapControll.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapControll : MonoBehaviour
+{
+    Vector3 pos;
+    Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        offset=new Vector3(0,2,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pos=new Vector3(0,0,0);
+        for(int i=0;i<GameManager.Instance.playersGenericObjectArray.ArrayLength();i++)
+        {
+            pos+=GameManager.Instance.playersGenericObjectArray.MemberWithIndex(i).transform.position+offset;
+        }
+        gameObject.transform.position=pos/GameManager.Instance.playersGenericObjectArray.ArrayLength();
+    }
+}
