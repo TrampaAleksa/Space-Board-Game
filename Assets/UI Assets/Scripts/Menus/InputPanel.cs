54bd1e81d5f8d08647c2a18e8708f1648da54401
Aleksa Trampa <alektramp@gmail.com>
2023-03-26 21:43:32 +0200

    added set player names via input fields

--- Assets/UI Assets/Scripts/Menus/InputPanel.cs
+++ Assets/UI Assets/Scripts/Menus/InputPanel.cs
@@ -10,26 +10,27 @@ namespace UI_Assets.Scripts
     public class InputPanel : MonoBehaviour
     {
         public InputField[] nameInputs;
-        
+
         public void StartGame()
         {
             if (InvalidInput())
                 return;
-            
+
             SetPlayerNames();
             SceneManager.LoadScene("Main Board game");
         }
-        
+
 
         private bool InvalidInput()
         {
             return IsAnyInputEmpty() || HasSameNames();
         }
-        
+
         private bool IsAnyInputEmpty()
         {
             return nameInputs.Any(inputField => string.IsNullOrEmpty(inputField.text));
         }
+
         private bool HasSameNames()
         {
             var nameSet = new HashSet<string>();
@@ -39,10 +40,12 @@ namespace UI_Assets.Scripts
             var numOfPlayers = nameInputs.Length;
             return nameSet.Count < numOfPlayers; // if there are duplicate names will return true
         }
-        
+
         private void SetPlayerNames()
         {
-            
+            for (int i = 0; i < BoardStateHolder.Instance.playerBoardStates.Length; i++)
+                BoardStateHolder.Instance.playerBoardStates[i].playerName =
+                    nameInputs[i].text;
         }
     }
 }
