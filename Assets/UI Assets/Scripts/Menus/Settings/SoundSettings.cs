d42927a51616cfd33aeae1fa6b46b0ee57870c0e
Aleksa Trampa <alektramp@gmail.com>
2023-03-27 23:09:40 +0200

    Added DB reduction for volume settings

--- Assets/UI Assets/Scripts/Menus/Settings/SoundSettings.cs
+++ Assets/UI Assets/Scripts/Menus/Settings/SoundSettings.cs
@@ -1,23 +1,53 @@
 using System;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 
 namespace UI_Assets.Scripts
 {
     public class SoundSettings : MonoBehaviour
     {
+        public AudioMixer masterMixer;
+        
         public Image SfxFillImage;
         public Image MusicFillImage;
         public float increment = 0.1f;
 
-        public void SlideSfxDown() => ChangeFillSliderValue(-increment, SfxFillImage);
-        public void SlideSfxUp() => ChangeFillSliderValue(+increment, SfxFillImage);
-        public void SlideMusicDown() => ChangeFillSliderValue(-increment, MusicFillImage);
-        public void SlideMusicUp() => ChangeFillSliderValue(+increment, MusicFillImage);
-
-        private void ChangeFillSliderValue(float value, Image fillImage)
+        private void Start()
         {
-            fillImage.fillAmount = Math.Clamp(fillImage.fillAmount + value, 0, 1);
+            AudioManager.Instance.PlaySound(AudioManager.MAIN_MENU_BACKGROUND,true,1f);
+        }
+
+        public void SlideSfxDown()
+        {
+            var newVolume = ChangeFillSliderValue(-increment, SfxFillImage);
+            AudioManager.Instance.PlaySound(AudioManager.SELECT,false,1f);
+
+            masterMixer.SetFloat("sfxVolume", -(1f - newVolume) * 80f);
+        }
+
+        public void SlideSfxUp()
+        {
+            var newVolume = ChangeFillSliderValue(+increment, SfxFillImage);
+            AudioManager.Instance.PlaySound(AudioManager.SELECT,false,1f);
+            masterMixer.SetFloat("sfxVolume", -(1f - newVolume) * 80f);
+        }
+
+        public void SlideMusicDown()
+        {
+            var newVolume = ChangeFillSliderValue(-increment, MusicFillImage);
+            masterMixer.SetFloat("musicVolume", -(1f - newVolume) * 80f);
+        }
+
+        public void SlideMusicUp()
+        {
+            var newVolume = ChangeFillSliderValue(+increment, MusicFillImage);
+            masterMixer.SetFloat("musicVolume", -(1f - newVolume) * 80f);
+        }
+
+        private float ChangeFillSliderValue(float value, Image fillImage)
+        {
+           return fillImage.fillAmount = Math.Clamp(fillImage.fillAmount + value, 0, 1);
         }
     }
 }
