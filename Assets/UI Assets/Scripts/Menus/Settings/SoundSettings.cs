4f8b30c0fd418906bbacb12d73ed5c979eb2f64d
Aleksa Trampa <alektramp@gmail.com>
2023-03-27 22:37:35 +0200

    Added code for sound settings

--- Assets/UI Assets/Scripts/Menus/Settings/SoundSettings.cs
+++ Assets/UI Assets/Scripts/Menus/Settings/SoundSettings.cs
@@ -1,9 +1,23 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace UI_Assets.Scripts
 {
     public class SoundSettings : MonoBehaviour
     {
-        
+        public Image SfxFillImage;
+        public Image MusicFillImage;
+        public float increment = 0.1f;
+
+        public void SlideSfxDown() => ChangeFillSliderValue(-increment, SfxFillImage);
+        public void SlideSfxUp() => ChangeFillSliderValue(+increment, SfxFillImage);
+        public void SlideMusicDown() => ChangeFillSliderValue(-increment, MusicFillImage);
+        public void SlideMusicUp() => ChangeFillSliderValue(+increment, MusicFillImage);
+
+        private void ChangeFillSliderValue(float value, Image fillImage)
+        {
+            fillImage.fillAmount += Math.Clamp(fillImage.fillAmount + value, 0, 1);
+        }
     }
 }
