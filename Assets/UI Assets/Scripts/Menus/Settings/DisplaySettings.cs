b1e8a777bb9e6077a702fe74111f9fc638ecd63c
Aleksa Trampa <alektramp@gmail.com>
2023-03-27 11:16:39 +0200

    Added Grab resolutions and display into settings

--- Assets/UI Assets/Scripts/Menus/Settings/DisplaySettings.cs
+++ Assets/UI Assets/Scripts/Menus/Settings/DisplaySettings.cs
@@ -1,9 +1,44 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace UI_Assets.Scripts
 {
     public class DisplaySettings : MonoBehaviour
     {
+        public int currentIndex;
+        public Text label;
         
+        private Resolution[] _resolutions;
+        
+        private void Start()
+        {
+            _resolutions = Screen.resolutions;
+            
+            foreach (var resolution in _resolutions)
+            {
+                if (resolution.width == Screen.currentResolution.width &&
+                    resolution.height == Screen.currentResolution.height) ;
+            }
+            
+            var lastResolutionIndex = _resolutions.Length - 1;
+            currentIndex = lastResolutionIndex;
+
+            label.text = ResolutionTextValue(CurrentResolution);
+        }
+
+        public void SlideValueDown()
+        {
+            
+        }
+        public void SlideValueUp()
+        {
+            
+        }
+        
+        
+        public string ResolutionTextValue(Resolution resolution) => resolution.width + " x " + resolution.height;
+        public Resolution CurrentResolution => _resolutions[currentIndex];
     }
+    
 }
