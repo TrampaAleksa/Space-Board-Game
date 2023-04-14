6653b2be1bcc1c11058b2d1be10412f449012046
Aleksa Trampa <alektramp@gmail.com>
2023-04-14 11:46:14 +0200

    cleanup of activity history handler after bug fix

--- Assets/Board Game Assets/Scripts/Board Game/Tooltip/Activity History/ActivityHistoryHandler.cs
+++ Assets/Board Game Assets/Scripts/Board Game/Tooltip/Activity History/ActivityHistoryHandler.cs
@@ -4,60 +4,37 @@ using UnityEngine;
 
 public class ActivityHistoryHandler : GenericObjectArray
 {
-
-    public Queue<ActivityHistoryTooltip> tooltips = new Queue<ActivityHistoryTooltip>();
+    private readonly Queue<ActivityHistoryTooltip> _tooltips = new Queue<ActivityHistoryTooltip>();
     public Transform verticalLayout;
     
     private void Awake()
     {
-        gameObjects = GameObject.FindGameObjectsWithTag("ActivityTooltip");
-        foreach (var tooltipObj in gameObjects)
-            tooltips.Enqueue(tooltipObj.GetComponent<ActivityHistoryTooltip>());
-        
+        foreach (var tooltipObj in GameObject.FindGameObjectsWithTag("ActivityTooltip"))
+            _tooltips.Enqueue(tooltipObj.GetComponent<ActivityHistoryTooltip>());
     }
 
     public ActivityHistoryHandler ShowActivityTooltipMessage(string message)
-    {
-        return HandleWithQueueImpl(message);
-        //return HandleWithGenericArrayImpl(message);
-    }
-
-    private ActivityHistoryHandler HandleWithQueueImpl(string message)
     {
         AddTooltipToBottomOfHistory(message);
         RefreshTooltipsLayout();
         return this;
     }
 
+    
     private void AddTooltipToBottomOfHistory(string message)
     {
-        var tooltipExitingHistory = tooltips.Dequeue();
-        tooltips.Enqueue(tooltipExitingHistory); // move the tooltip from the bottom to the top
+        var tooltipExitingHistory = _tooltips.Dequeue();
+        _tooltips.Enqueue(tooltipExitingHistory); // move the tooltip from the bottom to the top
 
         tooltipExitingHistory.ClearActivityText().AddTextToTooltip(message);
     }
 
     private void RefreshTooltipsLayout()
     {
-        foreach (var tooltip in tooltips)
+        foreach (var tooltip in _tooltips)
         {
             tooltip.transform.parent.transform.parent = null;
             tooltip.transform.parent.transform.parent = verticalLayout;
         }
-    }
-
-    private ActivityHistoryHandler HandleWithGenericArrayImpl(string message)
-    {
-        for (int i=0; i< ArrayLength()-1 ; i++){
-            TooltipWithIndex(i).ClearActivityText()
-                .AddTextToTooltip(TooltipWithIndex(i+1).TooltipText.text);
-        }
-        ActivityHistoryTooltip lastAHTooltip = LastMember().GetComponent<ActivityHistoryTooltip>();
-        lastAHTooltip.ClearActivityText().AddTextToTooltip(message);
-        return this;
-    }
-
-    private ActivityHistoryTooltip TooltipWithIndex(int i){
-        return MemberWithIndex(i).GetComponent<ActivityHistoryTooltip>();
     }
 }
