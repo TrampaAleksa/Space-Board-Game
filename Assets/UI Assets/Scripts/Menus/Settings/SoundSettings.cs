058d373964ec7a57c86da1e22f36bbb85ab4a764
Aleksa Trampa <alektramp@gmail.com>
2023-04-14 10:17:09 +0200

    Linearized sound settings sliders

--- Assets/UI Assets/Scripts/Menus/Settings/SoundSettings.cs
+++ Assets/UI Assets/Scripts/Menus/Settings/SoundSettings.cs
@@ -13,6 +13,8 @@ namespace UI_Assets.Scripts
         public Image MusicFillImage;
         public float increment = 0.1f;
 
+        public float volumeValueTest = 60f;
+        
         private void Start()
         {
             AudioManager.Instance.PlaySound(AudioManager.MAIN_MENU_BACKGROUND,true,1f);
@@ -20,34 +22,52 @@ namespace UI_Assets.Scripts
 
         public void SlideSfxDown()
         {
-            var newVolume = ChangeFillSliderValue(-increment, SfxFillImage);
+            var newVolumeScale = ChangeFillSliderValue(-increment, SfxFillImage);
+            var volumeLin = GetLinearizedVolumeScale(newVolumeScale);
+            
+            masterMixer.SetFloat("sfxVolume", volumeLin);
+            
             AudioManager.Instance.PlaySound(AudioManager.SELECT,false,1f);
-
-            masterMixer.SetFloat("sfxVolume", -(1f - newVolume) * 80f);
         }
 
         public void SlideSfxUp()
         {
-            var newVolume = ChangeFillSliderValue(+increment, SfxFillImage);
+            var newVolumeScale = ChangeFillSliderValue(+increment, SfxFillImage);
+            var volumeLin = GetLinearizedVolumeScale(newVolumeScale);
+            
+            masterMixer.SetFloat("sfxVolume", volumeLin);
+
             AudioManager.Instance.PlaySound(AudioManager.SELECT,false,1f);
-            masterMixer.SetFloat("sfxVolume", -(1f - newVolume) * 80f);
         }
 
         public void SlideMusicDown()
         {
-            var newVolume = ChangeFillSliderValue(-increment, MusicFillImage);
-            masterMixer.SetFloat("musicVolume", -(1f - newVolume) * 80f);
+            var newVolumeScale = ChangeFillSliderValue(-increment, MusicFillImage);
+            var volumeLin = GetLinearizedVolumeScale(newVolumeScale);
+
+            masterMixer.SetFloat("musicVolume", volumeLin);
         }
 
         public void SlideMusicUp()
         {
-            var newVolume = ChangeFillSliderValue(+increment, MusicFillImage);
-            masterMixer.SetFloat("musicVolume", -(1f - newVolume) * 80f);
+            var newVolumeScale = ChangeFillSliderValue(+increment, MusicFillImage);
+            var volumeLin = GetLinearizedVolumeScale(newVolumeScale);
+            
+            masterMixer.SetFloat("musicVolume", volumeLin);
         }
 
-        private float ChangeFillSliderValue(float value, Image fillImage)
+        private float ChangeFillSliderValue(float value, Image fillImage)  // 0-1 scale
         {
            return fillImage.fillAmount = Math.Clamp(fillImage.fillAmount + value, 0, 1);
+        }
+
+        private float GetLinearizedVolumeScale(float scale) // since sound mixers work with DB we need linearized sliders
+        {
+            var log = 40 * Mathf.Log10((scale * 100f) + 0.01f); // needs percentages from 0-100
+            var volumeLin = -(80 -log);
+            // var volumeDB = -(1f - newVolume) * 80f;
+
+            return volumeLin;
         }
     }
 }
